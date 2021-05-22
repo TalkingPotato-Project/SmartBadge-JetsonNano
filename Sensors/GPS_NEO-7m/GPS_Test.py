1d8839c8efe46fcb15b02974efde00ecd4ef21bd
JJinTae <46085058+JJinTae@users.noreply.github.com>
2021-05-23 05:28:48 +0900

    Implements requests GPS data to server
    - 임시 서버로 GPS 정보를 3초 간격으로 전송 확인

--- Sensors/GPS_NEO-7m/GPS_Test.py
+++ Sensors/GPS_NEO-7m/GPS_Test.py
@@ -1,17 +1,38 @@
 import os
-import time
 
-aa = 0
+import time
+import serial
+import adafruit_gps
+import requests
+import json
+
+uart = serial.Serial("/dev/ttyTHS1", baudrate=9600, timeout=30)
+smartBadgeID = 131313
+URL = 'http://112.158.50.42:9080/location/'+str(smartBadgeID)+'/'
+headers = {'Content-Type': 'application/json', 'user-agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0'}
+
+gps = adafruit_gps.GPS(uart)
+gps.send_command(b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+gps.send_command(b'PMTK220,1000')
+
+last_print = time.monotonic()
 
 while True:
-	aa += 1
-	try:
-		data = os.system("cat /dev/ttyTHS1")
-	except KeyboardInterrupt:
-		break
 
-	print("this time : ",aa)
-	print(data)
-	print("end data")
-	time.sleep(10)
+    gps.update()
 
+    current = time.monotonic()
+    if current - last_print >= 3.0:
+        last_print = current
+        if not gps.has_fix:
+            print('Waiting for fix...')
+            continue
+        print('=' * 40)  # Print a separator line.
+        print('Latitude: {0:.6f} degrees'.format(gps.latitude))
+        print('Longitude: {0:.6f} degrees'.format(gps.longitude))
+        data = {
+            "smartBadgeID":smartBadgeID,
+            "longitude":gps.longitude,
+            "latitude":gps.latitude
+        }
+        r = requests.put(URL, headers=headers, data=json.dumps(data))
